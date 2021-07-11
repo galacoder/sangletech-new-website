b11c937182bc8453efc6b3ce24a6d8991b0b50e0
onlineworld69 <onlineworld69@users.noreply.github.com>
2021-07-11 18:22:51 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: "Trang-Ch-ac363edd622a47bfa4d724ad90e879b8",
+  rootNotionPageId: "Sang-Le-Tech-Website-ac363edd622a47bfa4d724ad90e879b8",
 
   /// if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
