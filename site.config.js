70a030bac18491115d1eeaa5057b5cc45c27b918
Sang Le <lengobaosang@gmail.com>
2021-09-02 22:21:21 +0700

    update package.json and spaceid

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'Sang-Le-Tech-Website-ac363edd622a47bfa4d724ad90e879b8',
+  rootNotionPageId: 'ac363edd622a47bfa4d724ad90e879b8',
 
   /// if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '8197edf5-190e-47a8-aca1-926954bedb4f',
 
   // basic site info (required)
   name: 'Sang Le Tech Blog',
